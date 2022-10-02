8bac5ead3a643e83b4a82cdac0d8d8278c3778dd
HIRASHIKI RAFAEL <rafaelkishihiro@gmail.com>
2022-10-03 00:38:40 +0900

    verdadeiro 10-card_button

---
+++ components/CommonUI/ButtonIcon.tsx
@@ -0,0 +1,27 @@
+import styles from "./Button.module.css";
+import Image from "next/image";
+import cart from "../../public/icons/cart_add.png";
+
+const ButtonIcon = ({ buttonTxt, icon }: ButtonProps) => {
+  const iconSrc = getIcon(icon);
+
+  return (
+    <button className={styles.button}>
+      <span>{buttonTxt}</span>
+      <Image src={iconSrc} className={styles.icon} width={25} height={25} />
+    </button>
+  );
+};
+
+interface ButtonProps {
+  buttonTxt: string;
+  icon: string;
+}
+
+function getIcon(iconName: string) {
+  if (iconName == "cart") return cart;
+
+  return cart;
+}
+
+export default ButtonIcon;
